b5ef1eb356a99a3f3e9cd7a90d85c50665522f0e
AJ Harmond <ajharmond@gmail.com>
2019-03-27 15:53:24 -0600

    full user authentication

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { initMiddleware, } from 'devise-axios';
 import { BrowserRouter, } from 'react-router-dom';
 import { AuthProvider, } from './providers/AuthProvider';
 import 'semantic-ui-css/semantic.min.css';
+
+initMiddleware();
 
 ReactDOM.render(
   <AuthProvider>
